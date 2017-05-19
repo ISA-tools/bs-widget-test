6617c88f1e58147ba6d0b86fcba74305b1dea35c
zigur <massimorgon@gmail.com>
2017-05-19 18:09:03 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,35 @@
+/**
+ * @author Massimiliano Izzo
+ */
+
+const express = require('express'), port = process.env.PORT || 3000,
+    compression = require('compression'),
+    app = express(), path = require('path'),
+    fs = require('fs');
+    // Promise = require('bluebird'),
+    // readFile = Promise.promisify(require('fs').readFile),
+    // cheerio = require('cheerio'),
+    // generateSitemap = require('./scripts/generateSitemap'),
+    // sitemap = generateSitemap(),
+    // ISATAB_INDEX_FILE = 'isatab-index.json',
+    // INVESTIGATIONS_ID_REGEX = /^sdata/;
+
+const indexHtmlFile = path.resolve(__dirname, 'index.html');
+
+
+app.use(compression());
+
+// app.use('/assets', express.static(`${__dirname}/assets`));
+// app.use('/data', express.static(`${__dirname}/data`));
+// app.use('/isatab-index.json', express.static(`${__dirname}/isatab-index.json`));
+
+
+// handle every other route with index.html, which will contain
+// a script tag to your application's JavaScript file(s).
+app.get('*', function (req, res){
+    res.sendFile(indexHtmlFile);
+});
+
+app.listen(port, () => {
+    console.log(`ISA-explorer listening on port ${port}`);
+});
