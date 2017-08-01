caebac0ee91c07a6b97a4650fae86b6b1d8c62b5
zigur <massimorgon@gmail.com>
2017-08-01 17:39:57 +0100

    fixed styles and unmounting

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
     <div id="app">
         <h1>{{ title }}</h1>
-        <v-select :options="collections" :on-change="updateWidgetView"></v-select>
+        <v-select :options="collections" :on-change="updateWidgetView" placeholder="Please select a collection or recommendation"></v-select>
         <div id='widget'></div>
     </div>
 </template>
@@ -12,18 +12,31 @@ export default {
     props: ['title', 'collections', 'host', 'apiKey'],
 
     methods: {
-        updateWidgetView(collection)
-            // TODO add unmount method
-            const widget = BSCollection.default.widgets.singleViewWidget.new({
+        updateWidgetView(collection) {
+            this.widget && this.widget.unmount();
+            this.widget = BSCollection.default.widgets.singleViewWidget.new({
                 host: this.host,
                 apiKey: this.apiKey,
                 id: collection.value,
                 selector: '#widget'
             });
-            widget.render();
+            this.widget.render();
         }
     }
 }
 </script>
 
-<style lang="scss"></style>
+<style scoped lang="scss">
+$bs-head-color: #249acc;
+$header-font-size: 36px;
+
+#app {
+    margin: 10px 8px;
+}
+
+#app h1 {
+    color: $bs-head-color;
+    font-family: 'Helvetica Neue';
+    font-size: $header-font-size;
+}
+</style>
